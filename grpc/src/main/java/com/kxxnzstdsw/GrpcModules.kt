0bdb702da7bfba1c7d44516037e79f808ca89666
lbty <langbiantianya@hotmail.com>
2025-08-08 17:37:27 +0800

    feat: 去掉hello word

--- grpc/src/main/java/com/kxxnzstdsw/GrpcModules.kt
+++ grpc/src/main/java/com/kxxnzstdsw/GrpcModules.kt
@@ -1,6 +1,5 @@
 package com.kxxnzstdsw
 
-import com.kxxnzstdsw.server.HelloWorldService
 import com.kxxnzstdsw.server.SessionService
 import io.grpc.BindableService
 import org.koin.dsl.module
@@ -8,7 +7,7 @@ import org.koin.dsl.module
 object GrpcModules {
     val grpcModule = module {
         single<List<BindableService>> {
-            listOf(HelloWorldService(), SessionService())
+            listOf(SessionService())
         }
     }
 }
